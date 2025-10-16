8d63f5cd9b58dcb78a76376a39a54c46f88b061b
noorfikri <wildanachmadnoorfikri@gmail.com>
2025-10-16 11:58:22 +0700

    ENV Example For Deploy

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -175,15 +175,16 @@ class ItemController extends Controller
             $item->save();
 
             $item->stocks()->delete();
+            \Log::info($item->stocks());
 
-            foreach ($validated['stocks'] as $stockCombo) {
-                ItemStock::create([
-                    'item_id' => $item->id,
-                    'size_id' => $stockCombo['size_id'],
-                    'colour_id' => $stockCombo['colour_id'],
-                    'stock' => $stockCombo['stock'],
-                ]);
-            }
+            // #foreach ($validated['stocks'] as $stockCombo) {
+            //     ItemStock::create([
+            //         'item_id' => $item->id,
+            //         'size_id' => $stockCombo['size_id'],
+            //         'colour_id' => $stockCombo['colour_id'],
+            //         'stock' => $stockCombo['stock'],
+            //     ]);
+            // }
 
             return redirect()->route('items.index')->with('status', 'Barang dengan nama: ' . $item->name . ' berhasil diperbarui');
         } catch (\Exception $e) {
